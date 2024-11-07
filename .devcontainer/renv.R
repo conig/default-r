ee23cfbfaf6cc8adc902347799e2c734ff100a85
James Conigrave <james.conigrave@gmail.com>
2024-11-08 09:41:27 +1100

    add support for zsh

--- .devcontainer/renv.R
+++ .devcontainer/renv.R
@@ -2,7 +2,7 @@ options(install.opts = "--no-lock")
 
 # Initialize renv if the library is empty
 if (is.null(unlist(renv::status()$library))) {
-    renv::init(bare = TRUE)
+    renv::init()
 }
 
 # Try to restore the environment from the lockfile
